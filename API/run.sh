86f7aa9da69db8abf97027ba6cd843d97d2b12c0
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-03-21 15:30:52 +0100

    Chg: path to API/main.py in API/run.sh

--- API/run.sh
+++ API/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # A script to launch the flask application.
-FLASK_APP=$(readlink -f main.py)
+FLASK_APP=$(readlink -f API/main.py)
 FLASK_ENV=development
 export FLASK_APP
 export FLASK_ENV
