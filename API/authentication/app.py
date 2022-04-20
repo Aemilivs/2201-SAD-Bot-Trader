f89aff33fa462ffe015f62bf4a696b6607c41528
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-04-21 01:14:21 +0200

    Chg: improving the verify_password function in authentication/app.py

--- API/authentication/app.py
+++ API/authentication/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from my_resource import PrivateResource
 from flask_httpauth import HTTPBasicAuth
@@ -7,16 +7,19 @@ app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
 auth = HTTPBasicAuth()
 
+#TODO: Create database
+#TODO: Only store Hashes instead of plain text passwords
 USER_DATA = {
-    "Username": "password"
+    "username": "password"
 }
 
 
 @auth.verify_password
 def verify_password(username, password):
-    if not (username and password):
-        return False
-    return USER_DATA.get(username) == password
+    if username in USER_DATA and \
+            USER_DATA.get(username) == password:
+        return username
+
 
 # add all resources here
 api.add_resource(PrivateResource, '/private')
