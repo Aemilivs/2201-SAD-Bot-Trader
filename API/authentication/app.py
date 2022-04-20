fe7a3d5cbfccc1001481a88d7e5a3ea536e04cd0
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-04-21 00:16:01 +0200

    Chg: removing code redundancy

--- API/authentication/app.py
+++ API/authentication/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask_restful import Api
 from my_resource import PrivateResource
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
+auth = HTTPBasicAuth()
+
+USER_DATA = {
+    "Username": "password"
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
 
 # add all resources here
 api.add_resource(PrivateResource, '/private')
