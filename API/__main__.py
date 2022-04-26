670744999da8868a9b22f07edf144f5b296fd8b3
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-04-26 03:41:58 +0200

    Add: Authentication to health_controller.py

--- API/__main__.py
+++ API/__main__.py
@@ -1,14 +1,31 @@
 """This module is the main runtime of the SAD Bot Trader API"""
 from flask import Flask
 from flask_restful import Api
+from flask_httpauth import HTTPBasicAuth
 from .health.controllers.health_controller import health_blueprint
 from .trade_trees.controllers.trade_trees_controller import TradeTreesController
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.register_blueprint(health_blueprint)
 # pylint: disable=no-value-for-parameter
 app.register_blueprint(TradeTreesController().blueprint)
+
+
+# TODO: Create database
+# TODO: Only store Hashes instead of plain text passwords
+USER_DATA = {
+    "username": "password"
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in USER_DATA and \
+            USER_DATA.get(username) == password:
+        return username
+
 
 if __name__ == "__main__":
     Api(app.run())
--- API/health/controllers/health_controller.py
+++ API/health/controllers/health_controller.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 import flask
+from flask_httpauth import HTTPBasicAuth
 
 health_blueprint = Blueprint('health', __name__)
+auth = HTTPBasicAuth()
 
 
-@health_blueprint .route('/api/health', methods=['GET'])
+@health_blueprint.route('/api/health', methods=['GET'])
+@auth.login_required
 def health():
     return flask.jsonify({'healthy': True}), 200
