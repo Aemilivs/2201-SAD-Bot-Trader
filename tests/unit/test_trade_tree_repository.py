c0c5f44ae4973e15e3aef2edab7bde2bff3ace91
Nikolai Skrabets <skrabets@gmail.com>
2022-04-28 09:33:50 +0200

    Create test_trade_tree_repository.py
    Unit test template

---
+++ tests/unit/test_trade_tree_repository.py
@@ -0,0 +1,8 @@
+from API.trade_trees.repositories.trade_tree_repository import TradeTreeRepository
+
+def test_template ():
+    trade_tree = TradeTreeRepository('trade_tree_title', True, '2022-04-04', '2022-04-05')
+    assert trade_tree.title == 'trade_tree_title'
+    assert trade_tree.isActive == True
+    assert trade_tree.createdAt == '2022-04-04'
+    assert trade_tree.updatedAt == '2022-04-05'
