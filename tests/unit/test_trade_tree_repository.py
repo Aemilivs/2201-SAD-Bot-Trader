b4e1774a98bf527caaef6321f62b52b17a54afa8
skrabets <skrabets@users.noreply.github.com>
2022-04-28 07:34:19 +0000

    autopep8 action fixes

--- tests/unit/test_trade_tree_repository.py
+++ tests/unit/test_trade_tree_repository.py
@@ -1,8 +1,10 @@
 from API.trade_trees.repositories.trade_tree_repository import TradeTreeRepository
 
-def test_template ():
-    trade_tree = TradeTreeRepository('trade_tree_title', True, '2022-04-04', '2022-04-05')
+
+def test_template():
+    trade_tree = TradeTreeRepository(
+        'trade_tree_title', True, '2022-04-04', '2022-04-05')
     assert trade_tree.title == 'trade_tree_title'
-    assert trade_tree.isActive == True
+    assert trade_tree.isActive
     assert trade_tree.createdAt == '2022-04-04'
     assert trade_tree.updatedAt == '2022-04-05'
