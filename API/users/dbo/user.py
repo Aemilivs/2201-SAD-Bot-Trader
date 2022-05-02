942113f23643e90226ebba18824478d26488b25a
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-05-02 14:47:35 +0200

    Chg: removing unecessary code from user.py

--- API/users/dbo/user.py
+++ API/users/dbo/user.py
@@ -1,7 +1,7 @@
 import uuid
 from peewee import UUIDField, TimestampField, CharField, BooleanField, ForeignKeyField, IntegerField, BooleanField, TextField
 from API.common.db_schema import BaseModel
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 class User(BaseModel):
@@ -11,9 +11,3 @@ class User(BaseModel):
     isActive = BooleanField(null=False, index=False)
     createdAt = TimestampField(null=False, index=False)
     updatedAt = TimestampField(null=False, index=False)
-
-    def hash_password(self, password):
-        self.pwd_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
