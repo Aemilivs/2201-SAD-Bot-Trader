d4516b7b70b2bd8796f078633f61f6a94fa4cae0
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-05-02 15:13:09 +0200

    Add: draft of endpoint to create a new user

--- API/__main__.py
+++ API/__main__.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from flask_httpauth import HTTPBasicAuth
 from .health.controllers.health_controller import health_blueprint
 from .trade_trees.controllers.trade_trees_controller import TradeTreesController
+from .users.services.user_service import UserService
 
 auth = HTTPBasicAuth()
 app = Flask(__name__)
@@ -16,8 +17,9 @@ app.register_blueprint(TradeTreesController().blueprint)
 # TODO: Only store Hashes instead of plain text passwords
 # TODO: Add endpoint to create new users, might be done with blue prints
 @app.route('/create-user')
-def index():
-    return "Hello, {}!".format(auth.current_user())
+def new_user(self, payload):
+    UserService.post_user(payload)
+    return "The user has been added sucesfully"
 
 
 if __name__ == "__main__":
