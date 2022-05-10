5ecfbf0be68df82bccf21b6f7994e6e8dea4c286
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-05-10 10:51:12 +0200

    Chg: fixing the auth.py verification process

--- API/authentication/auth.py
+++ API/authentication/auth.py
@@ -12,7 +12,11 @@ auth = HTTPBasicAuth()
 @auth.verify_password
 def authenticate(username, password):
     result = UserService().get_user(username)
+    print(result[0].password_hash)
+    print(result[0].name)
+
+    print(check_password_hash(result[0].password_hash, password))
     if len(result) > 0:
-        if check_password_hash(result[0].password, password):
-            return result
+        if check_password_hash(result[0].password_hash, password):
+            return result[0].name
     return None
