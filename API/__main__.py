3deb55ba602124ea892ee9b7c7b7992275ac5a72
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-05-11 17:45:22 +0200

    Del: clearing unnecessary code blocks

--- API/__main__.py
+++ API/__main__.py
@@ -1,26 +1,17 @@
 """This module is the main runtime of the SAD Bot Trader API"""
 from flask import Flask
 from flask_restful import Api
-from flask_httpauth import HTTPBasicAuth
 from .health.controllers.health_controller import health_blueprint
 from .trade_trees.controllers.trade_trees_controller import TradeTreesController
 from .users.controllers.users_controller import UsersController
 
-auth = HTTPBasicAuth()
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.register_blueprint(health_blueprint)
 # pylint: disable=no-value-for-parameter
 app.register_blueprint(TradeTreesController().blueprint)
 app.register_blueprint(UsersController().blueprint)
-
-
-# TODO: Only store Hashes instead of plain text passwords
-# TODO: Add endpoint to create new users, might be done with blue prints
-# @app.route('/create-user')
-# def new_user():
-#     UserService.post_user(requests.pyload)
-#     return "The user has been added sucesfully"
 
 
 if __name__ == "__main__":
