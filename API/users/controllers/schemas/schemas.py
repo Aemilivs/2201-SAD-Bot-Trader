3d5972ea927c0eb90fe6997ec139736c471a9f1c
Aemilivs <Aemilivs@users.noreply.github.com>
2022-05-22 07:21:01 +0000

    autopep8 action fixes

--- API/users/controllers/schemas/schemas.py
+++ API/users/controllers/schemas/schemas.py
@@ -2,6 +2,7 @@ from apiflask import Schema
 from apiflask.fields import String, Date, Boolean, UUID, List, Nested
 from apiflask.validators import Length, OneOf
 
+
 class CreateUserTradeTreeAPISchema(Schema):
     name = String()
-    password = String() 
+    password = String()
