4ecab64cc7319e0b5f24b26fa3f905dab45b4c42
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-05-26 22:53:50 +0200

    Add: adapt the API launcher API/run.sh for Mac O compatibility

--- API/run.sh
+++ API/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # A script to launch the flask application.
-FLASK_APP="$(readlink -f API/__main__.py)"
+#running on Mac might be problematic, ensure greadlink is installed on MAC
+FLASK_APP="$(readlink -f API/__main__.py || greadlink -f API/__main__.py)"
 FLASK_ENV=development
 export FLASK_APP
 export FLASK_ENV
