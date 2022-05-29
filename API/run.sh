562cd08967d5bf93c8d5c40d0d3669e0f7b42ada
Feynman43 <74698134+Feynman43@users.noreply.github.com>
2022-05-29 22:00:43 +0200

    Revert "Fixing UI integration issues"

--- API/run.sh
+++ API/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # A script to launch the flask application.
-#running on Mac might be problematic, ensure greadlink is installed on MAC
-FLASK_APP="$(readlink -f API/__main__.py || greadlink -f API/__main__.py)"
+FLASK_APP="$(readlink -f API/__main__.py)"
 FLASK_ENV=development
 export FLASK_APP
 export FLASK_ENV
